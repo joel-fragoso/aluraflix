a9109c9895fc1eb30e57ff44ec3acccc828acc0a
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-27 12:20:17 -0300

    fix: global style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
   :root {
     --primary: #2a7ae4;
     --black: #000;
-    --blickLigter: #9e9e9e;
+    --blackLigter: #9e9e9e;
     --grayLight: #f5f5f5;
     --grayMedium: #e5e5e5;
     --white: #fff;
